05a1a515e17470885cae5bf75a92a6955377ed50
PaulPloumhans <72436553+PaulPloumhans@users.noreply.github.com>
2021-06-25 17:08:03 +0200

    updating test files

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,9 @@ app.use('/api', routes);
 app.listen(port, () => {
     console.log(`Server is now listening at http://localhost:${port}`);
 });
+
+const testJasmine = (a: number, b: number): number => {
+    return a + b;
+};
+
+export default testJasmine;
