5ade370df722161565fbcd2c2086c12d6a63329f
PaulPloumhans <72436553+PaulPloumhans@users.noreply.github.com>
2021-06-25 12:17:58 +0200

    Adding changes to reflect use of routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world');
+import express from 'express';
+import routes from './routes/index';
+const app = express();
+const port = 3000;
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+    console.log(`Server is now listening at http://localhost:${port}`);
+});
