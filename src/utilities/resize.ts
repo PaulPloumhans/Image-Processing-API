f4f538ac43caf5b5f42dcd4fa1b874944801031c
PaulPloumhans <72436553+PaulPloumhans@users.noreply.github.com>
2021-06-28 18:12:54 +0200

    Added a capability to use the package sharp to resize images, plus do some caching.

--- src/utilities/resize.ts
+++ src/utilities/resize.ts
@@ -1,18 +1,47 @@
 import sharp from 'sharp';
 import { promises as fsPromises } from 'fs';
+import { existsSync, mkdirSync } from 'fs';
 import path from 'path';
 
 const imagesInPath = 'images';
 const imagesOutRootPath = 'thumbnails';
 
-const resize = async (fileName: string, width: number, height: number) => {
+const resize = async (
+    fileName: string,
+    width: number,
+    height: number
+): Promise<string> => {
     const inputFile = path.join(imagesInPath, fileName + '.jpg');
     const outputFile = path.join(
         imagesOutRootPath,
         fileName + '_' + width + '_' + height + '.jpg'
     );
-
-    await sharp(inputFile).resize(width, height).toFile(outputFile);
+    let ret = '';
+    // check if imagesOutRootPath is there; if not, create it
+    if (existsSync(imagesOutRootPath) !== true) {
+        console.log(`Creating folder ${imagesOutRootPath}`);
+        if (mkdirSync(imagesOutRootPath) !== undefined) {
+            ret = `Cannot create folder ${imagesOutRootPath}`;
+            return ret;
+        }
+    }
+    // check if outputFile is there; if yes, return immediately
+    if (existsSync(outputFile)) {
+        console.log(`File ${outputFile} is already present`);
+        return ret;
+    } else {
+        try {
+            await sharp(inputFile).resize(width, height).toFile(outputFile);
+        } catch (err) {
+            ret = err.name + ' : ' + err.message;
+        }
+    }
+    return ret;
 };
 
-resize('halfdome', 300, 300);
+const callResize = async () => {
+    const msg = await resize('halfdome', 400, 500);
+    console.log('msg = ', msg);
+};
+
+callResize();
